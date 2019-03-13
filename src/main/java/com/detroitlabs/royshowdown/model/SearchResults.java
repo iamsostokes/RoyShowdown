9104bb4c07a1343861fb397efdb664acbac65f92
Omar Atcho <omar@Omars-MacBook-Pro.local>
2019-03-13 15:40:30 -0400

    Adds SearchResults Class

--- src/main/java/com/detroitlabs/royshowdown/model/SearchResults.java
+++ src/main/java/com/detroitlabs/royshowdown/model/SearchResults.java
@@ -1,4 +1,21 @@
 package com.detroitlabs.royshowdown.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SearchResults {
+    ArrayList<Job> SearchResultItems = new ArrayList<Job>();
+
+    @JsonProperty("SearchResultItems")
+    public ArrayList<Job> getSearchResultItems() {
+        return SearchResultItems;
+    }
+
+    @JsonProperty("SearchResultItems")
+    public void setSearchResultItems(ArrayList<Job> searchResultItems) {
+        SearchResultItems = searchResultItems;
+    }
 }
