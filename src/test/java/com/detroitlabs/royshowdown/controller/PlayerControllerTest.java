0677d18b4bd5e19ff92501ade6379d1db1dfd488
Jamez Stokes <jamezstokes@MacBook-Pro-7.local>
2019-03-15 14:42:33 -0400

    Adds testing for determine result for player 1 winning, player 2 winning, and tie.

--- src/test/java/com/detroitlabs/royshowdown/controller/PlayerControllerTest.java
+++ src/test/java/com/detroitlabs/royshowdown/controller/PlayerControllerTest.java
@@ -1,6 +1,7 @@
 package com.detroitlabs.royshowdown.controller;
 
 import com.detroitlabs.royshowdown.model.*;
+import org.junit.Test;
 
 import java.util.ArrayList;
 
@@ -9,7 +10,7 @@ import static org.junit.Assert.*;
 public class PlayerControllerTest {
 
     @org.junit.Test
-    public void determineWinner() {
+    public void shouldTestThatPlayerTwoWins() {
 
         PlayerController testController = new PlayerController();
 
@@ -54,6 +55,100 @@ public class PlayerControllerTest {
 
 
 
+
+
+    }
+
+    @Test
+    public void shouldTestThatPlayerOneWins() {
+
+        PlayerController testController = new PlayerController();
+
+        Game game1 = new Game();
+
+        Player player1 = game1.getPlayers().get(0);
+        Player player2 = game1.getPlayers().get(1);
+
+        //Start player 1
+        Job job1 = new Job();
+        JobInfo jobInfo1 = new JobInfo();
+
+        SalaryRange salaryRange1 = new SalaryRange();
+        salaryRange1.setMaximumRange("200.00");
+        ArrayList<SalaryRange> pay1 = new ArrayList<>();
+        pay1.add(salaryRange1);
+
+        jobInfo1.setPositionRemuneration(pay1);
+
+        job1.setMatchedObjectDescriptor(jobInfo1);
+
+        player1.setJob(job1);
+
+        //Start player 2
+        Job job2 = new Job();
+        JobInfo jobInfo2 = new JobInfo();
+
+        SalaryRange salaryRange2 = new SalaryRange();
+        salaryRange2.setMaximumRange("100.00");
+        ArrayList<SalaryRange> pay2 = new ArrayList<>();
+        pay2.add(salaryRange2);
+
+        jobInfo2.setPositionRemuneration(pay2);
+
+        job2.setMatchedObjectDescriptor(jobInfo2);
+
+        player2.setJob(job2);
+        testController.setCurrentGame(game1);
+
+        String actual = testController.determineWinner();
+        assertEquals("playerOneWins", actual);
+
+
+    }
+
+    @Test
+    public void shouldTestThatResultIsATie() {
+
+        PlayerController testController = new PlayerController();
+
+        Game game1 = new Game();
+
+        Player player1 = game1.getPlayers().get(0);
+        Player player2 = game1.getPlayers().get(1);
+
+        //Start player 1
+        Job job1 = new Job();
+        JobInfo jobInfo1 = new JobInfo();
+
+        SalaryRange salaryRange1 = new SalaryRange();
+        salaryRange1.setMaximumRange("100.00");
+        ArrayList<SalaryRange> pay1 = new ArrayList<>();
+        pay1.add(salaryRange1);
+
+        jobInfo1.setPositionRemuneration(pay1);
+
+        job1.setMatchedObjectDescriptor(jobInfo1);
+
+        player1.setJob(job1);
+
+        //Start player 2
+        Job job2 = new Job();
+        JobInfo jobInfo2 = new JobInfo();
+
+        SalaryRange salaryRange2 = new SalaryRange();
+        salaryRange2.setMaximumRange("100.00");
+        ArrayList<SalaryRange> pay2 = new ArrayList<>();
+        pay2.add(salaryRange2);
+
+        jobInfo2.setPositionRemuneration(pay2);
+
+        job2.setMatchedObjectDescriptor(jobInfo2);
+
+        player2.setJob(job2);
+        testController.setCurrentGame(game1);
+
+        String actual = testController.determineWinner();
+        assertEquals("tie", actual);
 
 
     }
